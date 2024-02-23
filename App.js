91c6e42afbaafb07cd6fc41ab28cb06feb0129dc
zhlhu322 <0322enter@gmail.com>
2024-02-23 21:04:37 +0800

    Hello RN

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>111119042李恩亞</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
